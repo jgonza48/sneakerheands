bfca025b19928633fb932b831e7951369c5b64a1
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-14 19:45:22 -0400

    Add files via upload

--- getsh.js
+++ getsh.js
@@ -1,10 +1,10 @@
 //This function makes a call to the sneakerheads api using javascript fetch
 //upon submitting the form, the function is called
 // this code is intentionally kept simple to follow (I hope)
-
 function getSH() {
     let numberSH = document.getElementById('number').value;
-
+    
+if(numberSH == ''){return false;}//Don't submit if "number" field is left blank
     let url = `https://api.sneakerheads.xyz/sneaker/${numberSH}`;
     fetch(url, {
             method: "GET",
@@ -16,39 +16,43 @@ function getSH() {
         .then((res) => {
             return res.json()
         })
-        .then((data) => {
+        .then((data) => {let nft = data;
+            console.log(nft.attributes);
 
-            let result = `<div class="card-wrapper">`;
+            //let result = `<span><strong>${nft.description}</strong></span><br>`;
+            let result = ``;
+            //for(x in data){console.log(x);}
+  
+            result += `<div class="card">
+                        <div class="card-img"><img src="https://storage.googleapis.com/nftimagebucket/tokens/0x8dc7b6ec6fafa36085ee9ec8e39112428d3360aa/preview/${numberSH}.png" alt="" title="sneakerhead ${numberSH}"></div>`;
+                        //https://storage.googleapis.com/nftimagebucket/tokens/0x8dc7b6ec6fafa36085ee9ec8e39112428d3360aa/preview/${numberSH}.png
+           
+            result += `<figcaption style="padding:4px;">${nft.name}</figcaption>`
 
-            Object.entries(data).forEach(([key, value]) => {
-
-                 // if (key == "description") { // I do not need this  data
-                //     result += `<span><strong></strong></span>`
-                // }
-                if (key == "image") {
-                    result += `<div class="card">
-                                 <div class="card-img"><img src="${value}" alt="" title="sneakerhead ${numberSH}"></div>`;
-                }
-                else if (key == "name") {
-                    result += `<div><span style="padding:4px;">${value}</span><br></div>`
-                }
-                else if (key == "attributes") {
-
-                    result += `<table>` //Start table and head row
-                    value.forEach((attr) => {
-                        result += `<tr><td id="tdleft">${attr.trait_type}</td><td id="tdright">${attr.value}</td></tr>`; // add trait_type as column headings row
-                    })
-
-                    
-                    result += `</table></div>`; // end row and table
-                }
+            result += `<table>`; //Start table and head row
+            (nft.attributes).forEach((attr) => {
+                result += `<tr><td id="tdleft">${attr.trait_type}</td><td id="tdright">${attr.value}</td></tr>`; // add trait_type as column headings row
             })
+
+            result += `</table></div>`; // end row and table
+          
             result += `</div>`; // close the "card-wrapper" div
-            document.getElementById('result').innerHTML += result; // send to html page
+            let oldCard = document.getElementById("result").firstChild;
+
+            let newCard = document.createElement("div");
+            newCard.classList.add("card-wrapper");
+            newCard.innerHTML = result;
+            let parentDiv = document.getElementById("result");
+            parentDiv.insertBefore(newCard, oldCard);
+
             document.getElementById('number').value = '';
-            console.log(result);
-        })
+         })
         .catch(err => console.log('Request Failed', err));
 
+    return false;
+}
+
+function clearPage(){
+    document.getElementById('result').innerHTML = '';
     return false;
 }
