63c5c06c9b5f91741e32a44d49c03f67e19fc117
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-17 10:33:05 -0400

    Update resize.js

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -5,16 +5,12 @@ function scrollFunction() {
     let button = document.getElementsByClassName("button");
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header1").style.fontSize = "2em";
-    document.getElementById("header2").style.margin-top = "40px";
-
     
     for (let i = 0; i < button.length; i++) {
         button[i].style.padding = "6px";
     }
   } else {
     document.getElementById("header1").style.fontSize = "4em";
-    document.getElementById("header2").style.margin-top = "60px";
-
     
     for (let i = 0; i < header1.length; i++) {
         button[i].style.padding = "12px";
