f8afba6f3ada7e114b1c8707009fb77a23ef9633
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-17 10:07:54 -0400

    Update resize
    change header2 margin top

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -5,12 +5,16 @@ function scrollFunction() {
     let header1 = document.getElementsByClassName("button");
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header1").style.fontSize = "2em";
+    document.getElementById("header2").style.margin-top = "40px";
+
     
     for (let i = 0; i < header1.length; i++) {
         header1[i].style.padding = "6px";
     }
   } else {
     document.getElementById("header1").style.fontSize = "4em";
+    document.getElementById("header2").style.margin-top = "60px";
+
     
     for (let i = 0; i < header1.length; i++) {
         header1[i].style.padding = "12px";
