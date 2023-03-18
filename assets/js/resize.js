ee9f00868807d6d335f3c9f2c716febf6819579f
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-18 10:24:57 -0400

    css changes

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -4,13 +4,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     let button = document.getElementsByClassName("button");
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header1").style.fontSize = "2em";
+    document.getElementById("header").style.fontSize = "2em";
     
     for (let i = 0; i < button.length; i++) {
         button[i].style.padding = "6px";
     }
   } else {
-    document.getElementById("header1").style.fontSize = "3.5em";
+    document.getElementById("header1").style.fontSize = "3em";
     
     for (let i = 0; i < header1.length; i++) {
         button[i].style.padding = "12px";
