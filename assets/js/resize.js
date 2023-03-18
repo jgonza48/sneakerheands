4e81557283357e2ac02f952a8d4180d40339e320
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-18 16:19:26 -0400

    font color

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -7,7 +7,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
   {
     header1.style.fontSize = "2em";
-    header1.style.color = "#000"; /* Fallback for older browsers */
+    header1.style.color = "#fff"; /* Fallback for older browsers */
     header1.style.color = rgba(255, 255, 255, 0.5);
 
     for (let i = 0; i < button.length; i++) { button[i].style.padding = "6px"; }
