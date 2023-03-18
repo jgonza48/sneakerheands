084bb927fab839a0fe917a4d5cc2ea6dd5089e15
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-18 19:53:31 -0400

    fixed bug in resize.js

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -4,22 +4,26 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     let button = document.getElementsByClassName("button");
     let header1 = document.getElementById("header1");
+    let numInput = document.getElementById("number");
+
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
   {
     header1.style.fontSize = "2em";
     header1.style.color = "rgba(255, 255, 255, 0.5)";
+    numInput.style.padding = " 6px 14px"
 
     for (let i = 0; i < button.length; i++) { 
-      button[i].style.padding = "6px"; 
+      button[i].style.padding = "6px 12px"; 
       button[i].style.background = "rgb(60, 136, 187, 0.4)";  }
   } 
   else {
         header1.style.fontSize = "3em";
         header1.style.color = "rgba(255, 255, 255)";
+        numInput.style.padding = " 10px 14px";
 
-        for (let i = 0; i < header1.length; i++) { 
-          button[i].style.padding = "12px";
-          button[i].style.background = "linear-gradient(-180deg, rgb(60, 136, 187), rgb(16, 52, 107));" 
+        for (let i = 0; i < button.length; i++) { 
+          button[i].style.padding = "12px 24px";
+          button[i].style.background = "rgb(38, 62, 105)"; 
         }
   }
 }
