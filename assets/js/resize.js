fb4bb004adf1f214e83715c03b53b7116b4945c8
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-18 16:53:22 -0400

    Update resize.js

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -9,11 +9,18 @@ function scrollFunction() {
     header1.style.fontSize = "2em";
     header1.style.color = "rgba(255, 255, 255, 0.5)";
 
-    for (let i = 0; i < button.length; i++) { button[i].style.padding = "6px"; button[i].style.background = "rgb(60, 136, 187, 0.4)";  }
+    for (let i = 0; i < button.length; i++) { 
+      button[i].style.padding = "6px"; 
+      button[i].style.background = "rgb(60, 136, 187, 0.4)";  }
   } 
   else {
         header1.style.fontSize = "3em";
-    
-        for (let i = 0; i < header1.length; i++) { button[i].style.padding = "12px"; }
+        header1.style.color = "rgba(255, 255, 255)";
+
+        for (let i = 0; i < header1.length; i++) { 
+          button[i].style.padding = "12px";
+          background-color = "initial";
+          background-image = "linear-gradient(-180deg, rgb(60, 136, 187), rgb(16, 52, 107));" 
+        }
   }
 }
