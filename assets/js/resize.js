a21ece4897d214dc7c7846d2d03ab2616e0e24f3
Juan Hundred <72508306+juan-hundred@users.noreply.github.com>
2023-03-18 15:57:38 -0400

    fix size bug

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -3,14 +3,16 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     let button = document.getElementsByClassName("button");
+    let header1 = document.getElementById("header1");
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "2em";
-    
+    header1.style.fontSize = "2em";
+    header1.style.color = "#000"; /* Fallback for older browsers */
+    header1.style.color = rgba(0, 0, 0, 0.5);
     for (let i = 0; i < button.length; i++) {
         button[i].style.padding = "6px";
     }
   } else {
-    document.getElementById("header1").style.fontSize = "3em";
+    document.getElementById("header1").style.fontSize = "2.5em";
     
     for (let i = 0; i < header1.length; i++) {
         button[i].style.padding = "12px";
